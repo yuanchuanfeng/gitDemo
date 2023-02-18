6f437ad732ab339a07ac1d166f406013a3424cf3
yuan <2449236107@qq.com>
2023-02-18 15:48:27 +0800

    second time

--- src/main/java/com/yuan/Git/GitTest.java
+++ src/main/java/com/yuan/Git/GitTest.java
@@ -7,5 +7,6 @@ package com.yuan.Git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello ");
+        System.out.println("hello2222 ");
     }
 }
