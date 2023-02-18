71082b8eb53e9b5a67435ad35f1ee1247a6585e7
yuan <2449236107@qq.com>
2023-02-18 15:27:43 +0800

    first time

---
+++ src/main/java/com/yuan/Git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yuan.Git;
+
+/**
+ * @author shkstart
+ * @create 2023-02-17 19:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+    }
+}
